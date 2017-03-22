d84155b49cc4f3c4020888582e5b59a0781b5501
Maxx Boehme <maxxboehme@gmail.com>
2017-03-21 22:58:50 -0500

    Adding version to SQLiteXX "0.1.0" Updated minimum cmake version to 3.1 Implemented start of creating cmake package configuration files
    For testing added the option to add memory leak testing to ctests with
    setting the flag "CTEST_MEMCHECK" to true.
    Making it so that DrMemory will download when building. Currently only
    works for Linux. Will need to add Windows support later

---
+++ cmake/External_DrMemory.cmake
@@ -0,0 +1,28 @@
+
+ExternalProject_Add(
+    DrMemory
+    # DEPENDS
+    TMP_DIR           ${CMAKE_BINARY_DIR}/temp
+    STAMP_DIR         ${CMAKE_BINARY_DIR}/stamp
+    #--Download step--------------
+    DOWNLOAD_DIR      ${download_dir}/DrMemory
+    URL               https://github.com/DynamoRIO/drmemory/releases/download/release_1.11.0/DrMemory-Linux-1.11.0-2.tar.gz
+    URL_HASH          SHA1=bdcaa95a117ae69a7fcf81094fe41f68f6d0189f
+    #--Update/Patch step----------
+    UPDATE_COMMAND    ""
+    PATCH_COMMAND     ""
+    CONFIGURE_COMMAND ""
+    #--Configure step-------------
+    SOURCE_DIR        ${CMAKE_CURRENT_BINARY_DIR}/DrMemory
+    #    CMAKE_ARGS        -DCMAKE_INSTALL_PREFIX:PATH=${DEPENDENCY_INSTALL_PREFIX}
+    #                      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    #--Build step-----------------
+    #    BINARY_DIR        ${CMAKE_BINARY_DIR}/DrMemory
+    BUILD_COMMAND     ""
+    #--Install step----------------
+    INSTALL_COMMAND   ""
+    INSTALL_DIR       ""
+)
+
+ExternalProject_Get_Property(DrMemory source_dir)
+set(DRMEMORY_COMMAND ${source_dir}/bin/drmemory CACHE INTERNAL "")
